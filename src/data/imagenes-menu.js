f90808dbd592011282679aac9b8e8e6c4ff478c0
agustin <info@raytecno.es>
2025-12-23 10:36:19 +0100

    Modificando WP

---
+++ src/data/imagenes-menu.js
@@ -0,0 +1,16 @@
+// src/data/imagenes-menu.js
+
+// Lista de imágenes del menú (sin las que tienen "Fondo")
+export const imagenesMenu = [
+  "City-MAR.jpg",
+  "Claddagh-MAR.jpg",
+  "Colecciones de Oro-LIN.jpg",
+  "Colecciones de Plata-LIN.jpg",
+  "Colgantes-LIN.jpg",
+  "Collares-LIN.jpg",
+  "Conexión-MAR.jpg",
+  // Añade más aquí...
+];
+
+export const STORAGE_MENU_URL =
+  "https://raygoldstorage.blob.core.windows.net/imagen-menu-web/";
